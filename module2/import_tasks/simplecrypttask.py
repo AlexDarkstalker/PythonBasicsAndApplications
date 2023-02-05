7c6a5c4c4394de3041a14d96612f7169dd5f57c6
Alex Seleznev <Alex.seleznev9407@gmail.com>
2023-02-05 17:38:03 +0600

    Imports stage finished. Changed paths a little

--- module2/import_tasks/simplecrypttask.py
+++ module2/import_tasks/simplecrypttask.py
@@ -1,16 +1,17 @@
 import urllib.request
 import os.path as os_path
+import os
 from simplecrypt import decrypt
 
 
-passwords_file_path = 'passwords.txt'
-encrypted_file_path = 'encrypted.bin'
+passwords_file_path = 'import_tasks/passwords.txt'
+encrypted_file_path = 'import_tasks/encrypted.bin'
 if not (os_path.exists(passwords_file_path) and os_path.exists(encrypted_file_path)):
-    urllib.request.urlretrieve('https://stepik.org/media/attachments/lesson/24466/encrypted.bin', 'encrypted.bin')
-    urllib.request.urlretrieve('https://stepik.org/media/attachments/lesson/24466/passwords.txt', 'passwords.txt')
-with open("encrypted.bin", "rb") as inp:
+    urllib.request.urlretrieve('https://stepik.org/media/attachments/lesson/24466/encrypted.bin', encrypted_file_path)
+    urllib.request.urlretrieve('https://stepik.org/media/attachments/lesson/24466/passwords.txt', passwords_file_path)
+with open(encrypted_file_path, "rb") as inp:
     encrypted = inp.read()
-with open("passwords.txt", "r", encoding='utf8') as passwords:
+with open(passwords_file_path, "r", encoding='utf8') as passwords:
     for line in passwords:
         try:
             print(line.strip())
